3cd1213c125eb3fbc5b0412691fa6014258fea2d
Mikael Holmgren <mikael.holmgren@mindinputtools.com>
2022-05-12 01:33:42 +0200

    Update speech

--- services/SpeechApi/State.cs
+++ services/SpeechApi/State.cs
@@ -7,33 +7,50 @@ namespace SpeechApi
     public static class State
     {
         public static ConcurrentQueue<QueueEntry> SpeechQueue { get; set; } = new();
-        public static ESpeakSynth Speaker = null;
+        public static Guid? SpeakingId = null;
+        public static bool IsSpeaking = false;
+        public static bool StopAll = false;
         public static bool QueueRunning = false;
         public static void ProcessQueue()
         {
             if (SpeechQueue.IsEmpty) return;
             bool done = false;
             State.QueueRunning = true;
+                    
+
             do
             {
+
+                
                 if (SpeechQueue.TryDequeue(out var queueEntry))
                 {
+                    using (var speaker = new ESpeakSynth()) {
                     var reset = new AutoResetEvent(false);
                     var completed = () =>
                     {
                         Console.WriteLine("Speech completed!");
+
                         reset.Set();
+                        IsSpeaking = false;
+                        SpeakingId = null;
+                        return;
                     };
-                    Speaker = new ESpeakSynth(completed);
-                    Speaker.Speak(queueEntry.Speak.Text);
+
+                    speaker.SetCompleted(completed);
+
+                    speaker.Speak(queueEntry.Speak.Text);
+                    IsSpeaking = true;
+                    SpeakingId = queueEntry.Id;
+
                     Console.WriteLine("Waiting for speech to complete..");
                     reset.WaitOne(TimeSpan.FromSeconds(120));
                     Console.WriteLine("Waiting done!");
-                    Speaker.Terminate();
+                }
                 }
                 else done = true;
             } while (!done);
             QueueRunning = false;
+
             Console.WriteLine("Finished processing speech queue");
         }
     }
