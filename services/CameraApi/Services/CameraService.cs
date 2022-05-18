f4bb9fa803f69d6ff157397b202a953b632e6c2b
Mikael Holmgren <mikael.holmgren@mindinputtools.com>
2022-05-18 13:00:18 +0200

    Retry get frame from camera if we get an empty frame

--- services/CameraApi/Services/CameraService.cs
+++ services/CameraApi/Services/CameraService.cs
@@ -26,6 +26,15 @@ namespace CameraApi.Services
         {
             if (!camera.Running) await camera.StartCamera();
             var mat = await camera.GetMatImage();
+            if (mat.Empty())
+            { // We need to retry. Can happen when camera just started.
+                int retryCount = 0;
+                do
+                {
+                    mat = await camera.GetMatImage();
+                } while (mat.Empty() && retryCount++ <3);
+                if (mat.Empty()) throw new Exception("Cannot get image from camera after retries..");
+            }
             var bytes = mat.ToBytes();
             return Results.File(bytes, "image/png");
         }
