dd29a9d7602a2eab446277a950a7f02992bdf628
Mikael Holmgren <mikael.holmgren@mindinputtools.com>
2022-05-20 21:34:13 +0200

    Usb Gadget files

---
+++ deployment_files/install_usb_gadget.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install dnsmasq
+sudo apt-get clean
+sudo cp dnsmasq_usb0 /etc/dnsmasq.d/usb0
+sudo cp network_interface_usb0 /etc/network/interfaces.d/usb0
+sudo cp usb-gadget.sh /usr/local/sbin/usb-gadget.sh
+sudo chmod +x /usr/local/sbin/usb-gadget.sh
+sudo cp usbgadget.service /lib/systemd/system/usbgadget.service
+sudo systemctl enable usbgadget.service
+sudo echo dtoverlay=dwc2 >> /boot/config.txt
+sudo sed -i 's/$/ modules-load=dwc2/' /boot/cmdline.txt
+sudo echo libcomposite >> /etc/modules
+sudo echo denyinterfaces usb0 >> /etc/dhcpcd.conf
+sudo systemctl enable getty@ttyGS0.service
