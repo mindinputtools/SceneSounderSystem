4449e9a63d445f852271dc1e44d96b28705b0d7e
Mikael Holmgren <mikael.holmgren@mindinputtools.com>
2022-05-22 15:46:42 +0200

    Added UI to toggle autospeak of objectdetection. + Rudimentary image preview

--- ui/WebUI/Services/ObjDetectYoloClient.cs
+++ ui/WebUI/Services/ObjDetectYoloClient.cs
@@ -15,5 +15,16 @@ namespace WebUI.Services
             string requestUri = $"/api/objdetect/yolo/?speak={speak}";
             return await httpClient.GetFromJsonAsync<IEnumerable<YoloPrediction>>(requestUri);
         }
+        public async Task StartYolo()
+        {
+            string requestUri = $"/api/objdetect/yolo/start";
+            await httpClient.GetAsync(requestUri);
+        }
+        public async Task StopYolo()
+        {
+            string requestUri = $"/api/objdetect/yolo/stop";
+            await httpClient.GetAsync(requestUri);
+        }
+
     }
 }
